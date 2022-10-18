dcaaba7e93f53f85500522e8eb46bd544286c1bd
ZaredMaldon <zaredosvaldom@gmail.com>
2022-10-18 01:02:34 -0500

    modelo.js
    se agrego una funcion

--- js/Model.js
+++ js/Model.js
@@ -1,1 +1,16 @@
-//aqui ira los metodos del modelo
+import * as THREE from "../librerias/three.module.js";
+import { GLTFLoader } from "../librerias/GLTFLoader";
+
+function CargarModeloGlb(scene,direccion,escalax,escalay,escalaz,posx,posy,posz) {
+    const loaderGLTF = new GLTFLoader();
+    loaderGLTF.load(
+    direccion,
+    function (gltf) {
+        let obj = gltf.scene;
+        obj.scale.set(escalax, escalay, escalaz);
+        obj.position.x=posx;
+        obj.position.y=posy;
+        obj.position.z=posz;
+        scene.add(obj);
+    });
+}
